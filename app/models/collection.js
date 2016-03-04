460e9f3871e9e384c20db9374156ec83a6a70ec5
NathanRomike <nathanromike@gmail.com>
2016-03-04 09:55:33 -0800

    Add new-collection and new-print components.

--- app/models/collection.js
+++ app/models/collection.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  date: DS.attr(),
+  artist: DS.attr(),
   description: DS.attr(),
   name: DS.attr(),
   prints: DS.hasMany('print', {async: true})
--- app/models/print.js
+++ app/models/print.js
@@ -1,11 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  artist: DS.attr(),
   collection: DS.belongsTo('collection', {async: true}),
+  date: DS.attr(),
   price: DS.attr(),
   size: DS.attr(),
   title: DS.attr(),
   type: DS.attr(),
+  url: DS.attr(),
   quantity: DS.attr(),
 });
