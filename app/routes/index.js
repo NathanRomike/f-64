1cd2e5e0b01a1359233f1ad841f931c073f287e7
NathanRomike <nathanromike@gmail.com>
2016-03-07 00:16:27 -0800

    Add sorting computed property.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   shoppingCart: Ember.inject.service(),
 
+  sortProperties: ['date:desc'],
+  sortedPrints: Ember.computed.sort('print', 'sortProperties'),
+
   model() {
     return this.store.findAll('print');
   },
